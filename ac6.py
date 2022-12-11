ecf69a53c51cb69b4f9c5e232cfdc5e75fa1fb37
Xavi_F <77384330+ArasaiX@users.noreply.github.com>
2022-12-11 10:47:07 +0100

    Add files via upload

--- ac6.py
+++ ac6.py
@@ -1,27 +1,13 @@
-import re
+### *** ADVENT OF CODE DAY 6 *** ###
+
+markerLenght = 14 #change this for part1 or two (4 or 14)
 
 f = open("input6.txt", "r")
-i = 0
-for element in f:
-    string = element
-for i in range(0, len(string)):
-    
-    first = string[i]
-    print(first)
-    second = string[i+1]
-    third = string[i+2]
-    fourth = string[i+3]
-    print(second,third,fourth)
-    if first == second or first == third or first == fourth or second == third or second == fourth or third == fourth:
-        print("finding...")
-    else:
-        print(i)
-        f = open("output6.txt", "a")
-        f.write(first)
-        f.write(second)
-        f.write(third)
-        f.write(fourth)
-        f.write("\n")
-        f.write(str(i))
-        f.close()
-    
+
+signal = f.read()
+
+for i in range (len(signal) - markerLenght - 1):
+    marker = signal[i:i+markerLenght]
+    if len(set(marker)) == markerLenght:
+        print("The first maker is:" + str(i+markerLenght))
+        break
